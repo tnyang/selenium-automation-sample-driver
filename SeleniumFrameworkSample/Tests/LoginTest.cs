fea60d814d7aef9d7f1513cc9233609addc28108
Tou Yang <tyang@healthgrades.com>
2017-01-04 14:41:16 -0700

    change to run googltest for saucelabstest

--- SeleniumFrameworkSample/Tests/LoginTest.cs
+++ SeleniumFrameworkSample/Tests/LoginTest.cs
@@ -158,273 +158,189 @@ namespace SeleniumFrameworkV2Sample
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive1()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive2()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive3()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive4()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive5()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive6()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive7()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive8()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive9()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive10()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive11()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive12()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive13()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive14()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive15()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive16()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive17()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive18()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive19()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive20()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
         [Test]
         [Category(MyTestCategory.SAUCELABS_TEST)]
         public void SauceLabValidateSuccessfulLoginPositive21()
         {
-            string userName = "mbadyelkhan@healthgrades.com";
-            string password = "Abcd1234!";
             var loginPage = new LoginPageObjects();
-            loginPage.OpenPage("/#/Login");
-            var homePage = loginPage.Login(userName, password);
-            Assert.IsTrue(homePage.isLogoLinkPresent(), "There is an issue with login.");
-            Log.Info("----- End of Positive test 2 -----\n\r");
+            loginPage.OpenGooglePage("/");
+            Log.Info("----- End of Positive GoogleTestForIE -----\n\r");
         }
 
     }
