da0651bd9325a3837d8701fbac8ee7d04629d8c7
Tou Yang <tyang@healthgrades.com>
2017-10-24 13:51:16 -0600

    add MainPage and OtherObject...

---
+++ SeleniumFrameworkSample/HtmlObjects/OtherObjectPage.cs
@@ -0,0 +1,23 @@
+﻿using SeleniumFrameworkV2Sample.PageObjects;
+using OpenQA.Selenium;
+using Healthgrades.TestAutomation.SeleniumFramework.Core;
+
+namespace SeleniumFrameworkV2Sample.HtmlObjects
+{
+    class OtherObjectPage : BasePageObjects
+    {
+        public OtherObjectPage() { }
+
+        public ButtonObject buttonObj = new ButtonObject("login", By.Id("button"));
+        public CheckboxObject checkboxObj = new CheckboxObject("checkbox", By.Id("checkbox"));
+        public DropDownListObject dropdownlistObj = new DropDownListObject("select", By.Id("select"));
+        public HyperlinkObject hyperlinkObj = new HyperlinkObject("link", By.Id("link"));
+        public ListBoxObject listboxObj = new ListBoxObject("select", By.Id("select"));
+        public PasswordTextFieldObject passwordTextfieldObj = new PasswordTextFieldObject("password", By.Id("password"));
+        public RadioButtonObject radioobj = new RadioButtonObject("radio", By.Id("radio"));
+        public TextFieldObject textfieldObj = new TextFieldObject("text", By.Id("text"));
+
+        public OtherObject buttonDemoText = new OtherObject("button_demo", By.Id("button_demo"));
+
+    }
+}
---
+++ SeleniumFrameworkSample/HtmlUnitTests/OtherObjectTests.cs
@@ -0,0 +1,53 @@
+﻿using Healthgrades.TestAutomation.SeleniumFramework.Core;
+using NUnit.Framework;
+
+namespace SeleniumFrameworkV2Sample
+{
+    [TestFixture]
+    public class OtherObjectTests : WebDriverTestBase
+    {
+
+        [Test]
+        [Category("OtherObjectTests")]
+        public void Click()
+        {
+            HtmlObjects.OtherObjectPage htmlOtherobject = new HtmlObjects.OtherObjectPage();
+            htmlOtherobject.OpenPage("/MainPage.html");
+
+            htmlOtherobject.buttonObj.Click();
+
+            string displaytext = htmlOtherobject.buttonDemoText.GetAttribute("innerHTML");
+            Assert.AreEqual(displaytext, "Hello World", "The otherobject Click() did not return the correct text: " + displaytext + " expected: " + "Hellow World");
+        }
+
+        [Test]
+        [Category("OtherObjectTests")]
+        public void GetSelectedElement()
+        {
+            HtmlObjects.OtherObjectPage htmlOtherobject = new HtmlObjects.OtherObjectPage();
+            htmlOtherobject.OpenPage("/MainPage.html");
+
+            string selected = htmlOtherobject.listboxObj.GetSelectedElement();
+
+            Assert.AreEqual(selected, "apple", "The otherobject GetSelectedElement() was value is wrong: " + selected + " expected: " + "apple");
+        }
+
+        [Test]
+        [Category("OtherObjectTests")]
+        public void SelectByText()
+        {
+            HtmlObjects.OtherObjectPage htmlOtherobject = new HtmlObjects.OtherObjectPage();
+
+            htmlOtherobject.OpenPage("/MainPage.html");
+
+            htmlOtherobject.listboxObj.SelectByText("cherry");
+
+            string selected = htmlOtherobject.listboxObj.GetSelectedElement();
+            Assert.AreEqual(selected, "cherry", "The otherobject SelectByText() was value is wrong: " + selected + " expected: " + "cherry");
+        }
+
+
+
+
+    }
+}
